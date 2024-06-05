2205b4660dbac79648ee9b039de9a1645e23fb46
Dara Ganesha Astarina <d.astarina@superus.de>
2024-06-05 11:55:20 +0200

    set global exception

---
+++ src/main/java/com/daraganesha/keep_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.daraganesha.keep_backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ItemNotFoundException.class)
+    public ResponseEntity<String> handleItemNotFoundException(ItemNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+}
